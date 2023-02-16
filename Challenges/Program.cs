7769a0c18d7fd09585dd6babd6f4700a08caab5e
jerikorir@gmail.com <jerikorir@gmail.com>
2023-02-16 08:57:46 +0300

    Add project files.

---
+++ Challenges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("................FizzBuzz.................");
+        for(int i=1; i<=100; i++)
+        {
+            Console.WriteLine(
+                (i%3 == 0)? "Fizz":
+                (i%5 == 0)? "Buzz":
+                (i % 15 == 0) ? "FizzBuzz" :
+                i
+                );
+        }
+    }
+}
